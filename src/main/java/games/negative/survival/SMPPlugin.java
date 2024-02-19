f9f68b2348de3be6f35e763a78ac0c252e4cfea6
Eric <negggative@gmail.com>
2024-02-19 15:59:57 -0400

    Add virtual crafting table item

--- src/main/java/games/negative/survival/SMPPlugin.java
+++ src/main/java/games/negative/survival/SMPPlugin.java
@@ -1,12 +1,22 @@
 package games.negative.survival;
 
 import games.negative.alumina.AluminaPlugin;
+import games.negative.alumina.builder.ItemBuilder;
+import games.negative.alumina.event.Events;
+import games.negative.alumina.util.NBTEditor;
+import games.negative.survival.core.Keys;
 import games.negative.survival.listener.CropListener;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.SmokingRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataType;
 import org.jetbrains.annotations.NotNull;
 
 public class SMPPlugin extends AluminaPlugin {
@@ -25,7 +35,7 @@ public class SMPPlugin extends AluminaPlugin {
         );
 
         SmokingRecipe rottenFleshToLeather = new SmokingRecipe(
-                new NamespacedKey(this, "rotten_flesh_to_leather"),
+                Keys.ROTTEN_FLESH_TO_LEATHER,
                 new ItemStack(Material.LEATHER),
                 Material.ROTTEN_FLESH,
                 1,
@@ -33,6 +43,38 @@ public class SMPPlugin extends AluminaPlugin {
         );
 
         Bukkit.getServer().addRecipe(rottenFleshToLeather);
+
+        ShapedRecipe virtualCraftingTable = new ShapedRecipe(
+                Keys.VIRTUAL_CRAFTING_TABLE,
+                new ItemBuilder(Material.CRAFTING_TABLE)
+                        .setName("&aVirtual Crafting Table &7(Right Click)")
+                        .applyPersistentData(data -> data.set(Keys.VIRTUAL_CRAFTING_TABLE, PersistentDataType.BOOLEAN, true))
+                        .build()
+        );
+
+        virtualCraftingTable.shape("LCL");
+        virtualCraftingTable.setIngredient('L', Material.LEATHER);
+        virtualCraftingTable.setIngredient('C', Material.CRAFTING_TABLE);
+
+        Bukkit.getServer().addRecipe(virtualCraftingTable);
+
+        Events.listen(PlayerInteractEvent.class, event -> {
+            Player player = event.getPlayer();
+            ItemStack item = event.getItem();
+            if (item == null || item.getType() != Material.CRAFTING_TABLE) return;
+
+            ItemMeta meta = item.getItemMeta();
+            if (meta == null) return;
+
+            if (!NBTEditor.has(meta, Keys.VIRTUAL_CRAFTING_TABLE)) return;
+
+            event.setCancelled(true);
+            event.setUseInteractedBlock(PlayerInteractEvent.Result.DENY);
+            event.setUseItemInHand(PlayerInteractEvent.Result.DENY);
+
+            player.playSound(player, Sound.UI_BUTTON_CLICK, 1, 1);
+            player.openWorkbench(null, true);
+        });
     }
 
     @Override
---
+++ src/main/java/games/negative/survival/core/Keys.java
@@ -0,0 +1,12 @@
+package games.negative.survival.core;
+
+import games.negative.survival.SMPPlugin;
+import org.bukkit.NamespacedKey;
+
+public class Keys {
+
+    public static final NamespacedKey ROTTEN_FLESH_TO_LEATHER = new NamespacedKey(SMPPlugin.instance(), "rotten_flesh_to_leather");
+
+    public static final NamespacedKey VIRTUAL_CRAFTING_TABLE = new NamespacedKey(SMPPlugin.instance(), "virtual_crafting_table");
+
+}
